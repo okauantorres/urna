a200fab9b7b83901e0e71f28f7b12d7599a8636a
Howlter <darqueboost@gmail.com>
2022-10-11 20:44:08 -0300

    Urna finalizada, porém corrige em andamento.

--- script.js
+++ script.js
@@ -8,8 +8,37 @@ let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0; // Para saber que a etapaAtual sempre vai começar com 0.
 let numero = ''; // Números que serão exibidos na tela
+let votoBranco = false;
+let votos = [];
 
 
+
+
+
+
+function comecarEtapa(){ // Começa a rodar o código para que seja iniciado a votação
+    let etapa = etapas[etapaAtual]; //etapa será etapas[0], ou seja, a etapa vereador no etapas.js
+
+    let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
+
+    for(let i=0;i<etapa.numeros;i++){ //Loop para ver a quantidade de numeros que tem na etapa.numeros =  5, de acordo com etapas.js
+        if(i === 0){// Quando i = 0 ou seja, primeiro número de voto, ele vai piscar
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else{ // Quando for diferente de zero, ele vai adicionar mais uma caixa de número
+            numeroHtml += '<div class="numero"></div>';
+        }
+    }
+
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    desc.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
 
 function atualizaInterface(){ // Atualiza interface, para que apareça o cargo, informações certas (após votar.)
     let etapa = etapas[etapaAtual];
@@ -29,7 +58,11 @@ function atualizaInterface(){ // Atualiza interface, para que apareça o cargo, 
 
     let fotosHtml = '';
     for(let i in candidato.fotos){
-        fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+        if(candidato.fotos[i].small){
+            fotosHtml += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+        } else{
+            fotosHtml += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+        }
     }
 
     lateral.innerHTML = fotosHtml;
@@ -41,32 +74,6 @@ function atualizaInterface(){ // Atualiza interface, para que apareça o cargo, 
 
 }
 
-
-
-function comecarEtapa(){ // Começa a rodar o código para que seja iniciado a votação
-    let etapa = etapas[etapaAtual]; //etapa será etapas[0], ou seja, a etapa vereador no etapas.js
-
-    let numeroHtml = '';
-    numero = '';
-
-
-    for(let i=0;i<etapa.numeros;i++){ //Loop para ver a quantidade de numeros que tem na etapa.numeros =  5, de acordo com etapas.js
-        if(i === 0){// Quando i = 0 ou seja, primeiro número de voto, ele vai piscar
-            numeroHtml += '<div class="numero pisca"></div>';
-        } else{ // Quando for diferente de zero, ele vai adicionar mais uma caixa de número
-            numeroHtml += '<div class="numero"></div>';
-        }
-    }
-
-
-    seuVotoPara.style.display = 'none';
-    cargo.innerHTML = etapa.titulo;
-    desc.innerHTML = '';
-    aviso.style.display = 'none';
-    lateral.innerHTML = '';
-    numeros.innerHTML = numeroHtml;
-}
-
 function clicou(n){ //Função de clicar em n
     let elNumero = document.querySelector('.numero.pisca'); //Seleciona a caixa do pisca
     if(elNumero !== null){ // Se for diferente de vazio
@@ -74,6 +81,9 @@ function clicou(n){ //Função de clicar em n
         numero = `${numero}${n}`; // Adiciona o n a variavel numero lá em cima, com o número que estava antes + n.
 
         elNumero.classList.remove('pisca'); //Logo após remove o pisca
+
+        // PARA O CORRIGE: Provavelmente teria que criar um else if dizzendo que se elnumero === null, ele tem que voltar uma casa;
+
         if(elNumero.nextElementSibling !== null){ //Se a div mais próxima for diferente de nada
             elNumero.nextElementSibling.classList.add('pisca'); //Insere o pisca na div mais próxima (próxima caixa de número)
         } else {
@@ -83,13 +93,65 @@ function clicou(n){ //Função de clicar em n
     }
 }
 function branco(){
-    alert("Clicou em branco");
+    if(numero === ''){
+        votoBranco = true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        desc.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+        lateral.innerHTML = '';
+    } else{
+        alert("Para votar em branco, nenhum número deve ser inserido. Corrija.")
+    }
 }
 function corrige(){
-console.log(numero);
+    let result = Array.isArray(newNumero);
+    if(result){
+        console.log("Error!");
+    } else{
+        var newNumero = numero.split('');
+        newNumero.pop();
+        newNumero = newNumero.toString();
+        newNumero = newNumero.replace(/,/g, '')
+        numero = newNumero; //Retorno do número com a última unidade já deletada, em forma de string.
+    }
+    //clicou('');  //AGORA EU TERIA QUE ATUALIZAR O numeroHtml, para que assim seja atualizada a interface
+
+    console.log(numero);  
+    console.log(newNumero); 
+    
+
 }
 function confirma(){
-    alert("Clicou em confirma");
+    let votoConfirmado = false;
+
+    let etapa = etapas[etapaAtual];
+    if(votoBranco === true){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        })
+        console.log('Confirmando como branco!');
+    } else if(numero.length === etapa.numeros){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        })
+        console.log('Confirmando como'+ numero);
+    }
+
+    if(votoConfirmado){
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa();
+        } else{
+            document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
+            console.log(votos);
+        }
+        
+    }
 }
 
 comecarEtapa();
